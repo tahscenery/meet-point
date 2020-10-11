b6e8728ef7d0c828fc8e9afbc47160b09c95a295
tahscenery <tahscenery@gmail.com>
2020-10-11 21:20:37 +1100

    Removed Svelte package

--- packages/client-svelte-rollup/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-import Form from "./Form.svelte";
-import Header from "./Header.svelte";
-import Theme from "./Theme.svelte";
-
-export { Form, Header, Theme };
--- packages/client-svelte-rollup/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import About from "./About.svelte";
-import Home from "./Home.svelte";
-import Login from "./Login.svelte";
-import NotFound from "./NotFound.svelte";
-import Register from "./Register.svelte";
-
-export { About, Home, Login, NotFound, Register };
